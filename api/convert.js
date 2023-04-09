b064aab2b26d3d20820afe30b3ce53c8648dec81
Chris <104967421+ChrisJAdamek@users.noreply.github.com>
2023-04-09 15:17:27 +1000

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -3,15 +3,47 @@ const mammoth = require('mammoth');
 const MarkdownIt = require('markdown-it');
 const multer = require('multer');
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
 const app = express();
 const upload = multer({ storage: multer.memoryStorage() });
 const md = new MarkdownIt();
 
+const generateJwtToken = () => {
+  const privateKey = Buffer.from(
+    process.env.ADOBE_PRIVATE_KEY_BASE64,
+    'base64'
+  ).toString();
+  const publicCert = Buffer.from(
+    process.env.ADOBE_PUBLIC_CERT_BASE64,
+    'base64'
+  ).toString();
+
+  const payload = {
+    iss: process.env.ADOBE_API_KEY,
+    sub: process.env.ADOBE_CLIENT_ID,
+    aud: 'https://ims-na1.adobelogin.com/c/',
+    exp: Math.round(Date.now() / 1000) + 60 * 60, // Expires in 1 hour
+  };
+
+  const token = jwt.sign(payload, privateKey, {
+    algorithm: 'RS256',
+    header: {
+      'x5c': [publicCert],
+      'x5t#S256': process.env.ADOBE_X5T,
+    },
+  });
+
+  return token;
+};
+
 app.post('/api/convert', upload.single('pdf'), async (req, res) => {
   const { buffer: pdfBuffer } = req.file;
 
   try {
+    const jwtToken = generateJwtToken();
+
     // Convert PDF to DOCX using Adobe API
     const response = await axios.post(
       'https://api.adobe.io/convert/pdf/export',
@@ -20,6 +52,7 @@ app.post('/api/convert', upload.single('pdf'), async (req, res) => {
         headers: {
           'Content-Type': 'application/pdf',
           'x-api-key': process.env.ADOBE_API_KEY,
+          'Authorization': `Bearer ${jwtToken}`,
         },
         responseType: 'arraybuffer',
       }
