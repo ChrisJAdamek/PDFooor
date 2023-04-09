9d9aaec0a28391c01a3d82aa2657e4e081c2436c
Chris <104967421+ChrisJAdamek@users.noreply.github.com>
2023-04-09 16:00:50 +1000

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
+const qs = require('querystring');
 
 const app = express();
 const upload = multer({ storage: multer.memoryStorage() });
@@ -21,10 +22,10 @@ const generateJwtToken = () => {
   ).toString();
 
   const payload = {
-    iss: process.env.ADOBE_CLIENT_ID, // Use the Client ID here
+    iss: process.env.ADOBE_CLIENT_ID,
     sub: process.env.ADOBE_CLIENT_ID,
     aud: 'https://ims-na1.adobelogin.com/c/',
-    exp: Math.round(Date.now() / 1000) + 60 * 60, // Expires in 1 hour
+    exp: Math.round(Date.now() / 1000) + 60 * 60,
   };
 
   const token = jwt.sign(payload, privateKey, {
@@ -37,22 +38,51 @@ const generateJwtToken = () => {
   return token;
 };
 
+const getAccessToken = async (jwtToken) => {
+  try {
+    const requestBody = qs.stringify({
+      client_id: process.env.ADOBE_CLIENT_ID,
+      client_secret: process.env.ADOBE_CLIENT_SECRET,
+      jwt_token: jwtToken,
+    });
+
+    const response = await axios.post(
+      'https://ims-na1.adobelogin.com/ims/exchange/jwt',
+      requestBody,
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    );
+
+    return response.data.access_token;
+  } catch (error) {
+    console.error('Error obtaining access token:', error);
+    return null;
+  }
+};
 
 app.post('/api/convert', upload.single('pdf'), async (req, res) => {
   const { buffer: pdfBuffer } = req.file;
 
   try {
     const jwtToken = generateJwtToken();
+    const accessToken = await getAccessToken(jwtToken);
 
-    // Convert PDF to DOCX using Adobe API
+    if (!accessToken) {
+      res.status(500).send('An error occurred while obtaining the access token.');
+      return;
+    }
+
     const response = await axios.post(
       'https://api.adobe.io/convert/pdf/export',
       pdfBuffer,
       {
         headers: {
           'Content-Type': 'application/pdf',
-          'x-api-key': process.env.ADOBE_CLIENT_ID, // Use the Client ID here
-          'Authorization': `Bearer ${jwtToken}`,
+          'x-api-key': process.env.ADOBE_CLIENT_ID,
+          'Authorization': `Bearer ${accessToken}`,
         },
         responseType: 'arraybuffer',
       }
@@ -60,11 +90,9 @@ app.post('/api/convert', upload.single('pdf'), async (req, res) => {
 
     const docxBuffer = Buffer.from(response.data);
 
-    // Convert DOCX to Markdown
     const result = await mammoth.convertToMarkdown({ buffer: docxBuffer });
     const markdown = result.value;
 
-    // Convert Markdown to HTML
     const html = md.render(markdown);
     res.send(html);
   } catch (error) {
